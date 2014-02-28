61dca86000232cee9bc75d1f6e23552374aeb9ec
Peter Gicking <pgicking@gmail.com>
2014-02-27 16:41:51 -0800

    added calcavg and changed main. Broken

--- main.py
+++ main.py
@@ -35,12 +35,21 @@ def wordLen(text):
             d[len(word)] = 1
     return d
 
+def calcAvg(d):
+    i = 0.0
+    j = 0.0
+    for x in d:
+        i += d[x]*x
+        j += x    
+    avg = (i / j)
+    return avg
+
 #This takes a list input and shuffles it and then takes the first j elements
 #This is so we dont accidently "sample" from the same position twice
-def samplePop(text):
+def samplePop(text,sampleSize):
     sample = []
     random.shuffle(text)
-    i = 25
+    i = sampleSize
     j = 0
     dupe = []
     while(j <= i):
@@ -50,6 +59,7 @@ def samplePop(text):
     return sample
 
 #Takes a subsample from the text to be iterated over
+#Creates random numbers, then goes to the index to stores it
 def subSample(text):
     idx = npr.randint(1,len(text),(len(text)))
     subSample = []
@@ -59,35 +69,46 @@ def subSample(text):
     return subSample
 
 #creates a histogram of the dictionary passed in
-def histoGram(d):
+def histogram(d,name):
     pyplot.bar(d.keys(), d.values())
-    pyplot.title( 'Bootstrapping')
+    pyplot.title( 'Bootstrapping - ' + name )
     pyplot.xlabel( 'Word length' )
     pyplot.ylabel ( 'Number of Occurences' )
-    pyplot.savefig( 'foo.png' )
-    pyplot.show()
+    pyplot.savefig( 'Bootstrap_' + name + '.pdf' )
+#    pyplot.show()
     
-def test():
-    X = range(0,100)
-    Y = [ i*i for i in X ]
-    
-    pyplot.plot(X,Y, '-')
-    pyplot.title( 'Plotting x*x' )
-    pyplot.xlabel( 'X Axis' )
-    pyplot.ylabel( 'Y Axis' )
-    pyplot.savefig( 'Simple.png' )
-    pyplot.show()
 
 #Main function
 def main():
-    sweetBook = readBook()
-    hissyGram = wordLen(sweetBook)
-    sample = samplePop(sweetBook)
-    bootstrap = subSample(sample)
-    subhissygram = wordLen(bootstrap)
-    histoGram(subhissygram)
-    i = len(sweetBook)
-    print i
+    name = 'Population'
+    #read book and store it in population
+    pop = readBook()
+    #Sort by word length and store in popHist
+    popHist = wordLen(pop)
+    #Creates the population histogram
+    #histogram(popHist,name)
+    print '\nCreating Histogram of the Population\n'
+   
+    sampleNum = int(raw_input("\nHow big of a subsample would you like?:"))
+    sample = samplePop(pop,sampleNum)    
+    bootNum = int(raw_input("\nHow many times would you like to bootstrap?:"))
+    i = 0
+    j = bootNum
+    bootStrap = {}
+    d = {}
+    while(i < j):
+        i += 1        
+        bootStrap = subSample(sample)
+        words = wordLen(bootStrap)
+        avg = calcAvg(words)
+        round(avg)
+        if avg in d:
+            d[avg] += 1
+        else:
+            d[avg] = 1
+    name = 'SubSample'
+    histogram(d,name)        
+    print d
     print("fuck")
 
 #Executes main
